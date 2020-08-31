3b221e73e69c2e7e9dc205b101a6ec2383ef08e2
jonkmatsumo <61896186+jonkmatsumo@users.noreply.github.com>
2020-08-30 17:54:39 -0700

    Bug Fix

---
+++ README.md
@@ -0,0 +1,2 @@
+# learning_visualizer
+Interactive web application that compares the effectiveness of different machine learning algorithms.
--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 import plotly.express as px
+from scipy.stats import zscore
 
 DATA_URL = (
-"/home/rhyme/Desktop/Project/Motor_Vehicle_Collisions_-_Crashes.csv"
+"Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 
 st.title("Motor Vehicle Collisions in New York City")
@@ -19,22 +20,23 @@ def load_data(nrows):
     lowercase = lambda x : str(x).lower()
     data.rename(lowercase, axis = 'columns', inplace=True)
     data.rename(columns={'crash_date_crash_time': 'date/time'}, inplace=True)
+
     return data
 
 data = load_data(100000)
 original_data = data
 
 st.header("Where are the most people injured in NYC?")
-injured_people = st.slider("Number of persons injured in vehicle collisions", 0, 19)
-st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
+injured_people = st.slider("Number of persons injured in vehicle collisions", 0, 19, 4)
 
+midpoint = (np.average(data['latitude']), np.average(data['longitude']))
+st.map(data.query("injured_persons >= @injured_people")[["latitude", "longitude"]].dropna(how="any"))
 
 st.header("How many collisions occur during a given time of day?")
 hour = st.slider("Hour to look at", 0, 23)
 data = data[data['date/time'].dt.hour == hour]
 
 st.markdown("Vehicle collisions between %i:00 and %i:00" % (hour, (hour + 1) % 24))
-midpoint = (np.average(data['latitude']), np.average(data['longitude']))
 
 
 st.write(pdk.Deck(
